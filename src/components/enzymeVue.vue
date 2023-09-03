65faf3f085692330db33138cc8059ae362f3b6d5
Jerry_TJU <1422768678@qq.com>
2023-09-03 12:39:51 +0800

    first commit

---
+++ src/components/enzymeVue.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="enzyme">
+        <img src="@/assets/enzyme.svg"/>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+img{
+    height:50px;
+}
+</style>
